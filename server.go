f90954d829e076079a9e8e60fd882b0f04c0737c
Jesús Gallego Irles <jesusgallego@outlook.com>
2016-04-09 10:58:25 +0200

    login
    handler that checks if the user is in the db

--- server.go
+++ server.go
@@ -1,99 +1,125 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
-    "golang.org/x/net/websocket"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "secure-chat/src"
-    "strconv"
+	"golang.org/x/net/websocket"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"net/http"
+	"os"
+	"project/server/src"
+	"strconv"
 )
 
-// Get the Port from the environment so we can run on Heroku
+// GetPort Get the Port from the environment so we can run on Heroku
 func GetPort() string {
-    var port = os.Getenv("PORT")
-    // Set a default port if there is nothing in the environment
-    if port == "" {
-        port = "8080"
-        log.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-    }
-    return ":" + port
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8080"
+		log.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
 
-
-/*
 // User structure
 type User struct {
-    ID       bson.ObjectId `bson:"_id,omitempty"`
-    Username     string        `bson:"username"`
-    Password string        `bson:"password"`
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	Username string        `bson:"name"`
+	Password string        `bson:"password"`
 }
-*/
+
 // Message structure
 type Message struct {
-    ID       bson.ObjectId `bson:"_id,omitempty"`
-    Content     string        `bson:"content"`
+	ID      bson.ObjectId `bson:"_id,omitempty"`
+	Content string        `bson:"content"`
 }
 
+func mongo(message string) string {
+	session, err := mgo.Dial(src.URI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
 
-func mongo(message string) string{
-    session, err := mgo.Dial(src.URI)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer session.Close()
-    session.SetMode(mgo.Monotonic, true)
+	// Collection messages
+	c := session.DB(src.AUTH_DATABASE).C("messages")
 
-    // Collection messages
-    c := session.DB(src.AUTH_DATABASE).C("messages")
+	// Insert
+	result := Message{}
+	err = c.Insert(&Message{Content: message})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Insert
-    result := Message{}
-    err = c.Insert(&Message{Content: message})
-    if err != nil{
-        log.Fatal(err)
-    }
+	// Query count
+	messages, err := c.Count()
+	// Query
+	//err = c.Find(bson.M{"username": "david"}) .One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(result)
 
-    // Query count
-    messages, err := c.Count()
-    // Query
-    //err = c.Find(bson.M{"username": "david"}) .One(&result)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Println(result)
-        
-    // fmt.Println("Id: ", result.ID, " - User: ", result.Name, " - Password: ", result.Password)
-    response := "Total messages =" + strconv.Itoa(messages)
-    return response
+	// fmt.Println("Id: ", result.ID, " - User: ", result.Name, " - Password: ", result.Password)
+	response := "Total messages =" + strconv.Itoa(messages)
+	return response
 }
-
 
 func chatHandler(ws *websocket.Conn) {
-    msg := make([]byte, 512)
-    n, err := ws.Read(msg)
-    if err != nil {
-        log.Fatal(err)
-    }
-    message := string(msg[:n])
-    log.Printf("Receive: %s\n", message)
+	msg := make([]byte, 512)
+	n, err := ws.Read(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	message := string(msg[:n])
+	log.Printf("Receive: %s\n", message)
 
-    response := mongo(message)
+	response := mongo(message)
 
-    _, err = ws.Write([]byte(response))
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Printf("Send: %s\n", response)
+	_, err = ws.Write([]byte(response))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Send: %s\n", response)
+}
+
+func searchUser(user, passwd string) bool {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("securechat").C("user")
+
+	result := User{}
+
+	err = c.Find(bson.M{"name": user, "password": passwd}).One(&result)
+
+	if err != nil {
+		log.Println("error count", err)
+		return false
+	}
+
+	return result.ID.Valid()
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request: ", r.FormValue("user"), r.FormValue("pass"))
+	logged := searchUser(r.FormValue("user"), r.FormValue("pass"))
+
+	w.Write([]byte(strconv.AppendBool(make([]byte, 0), logged)))
 }
 
 func main() {
-    http.Handle("/chat", websocket.Handler(chatHandler))
-    port := GetPort()
-    err := http.ListenAndServe(port, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: " + err.Error())
-    }
+	http.Handle("/chat", websocket.Handler(chatHandler))
+	http.HandleFunc("/login", loginHandler)
+	port := GetPort()
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: " + err.Error())
+	}
 }
--- src/load_env.go
+++ src/load_env.go
@@ -1,8 +1,25 @@
 package src
 
 import (
-    "os"
+	"os"
 )
 
-var URI = os.Getenv("MONGOLAB_URI")
-var AUTH_DATABASE = os.Getenv("MONGOLAB_DB")
+var develop = os.Getenv("PROD")
+
+var URI = getURI()
+var AUTH_DATABASE = getDatabase()
+
+func getURI() string {
+	if develop == "true" {
+		return os.Getenv("MONGOLAB_URI")
+	}
+	return "localhost:27017"
+
+}
+
+func getDatabase() string {
+	if develop == "true" {
+		return os.Getenv("MONGOLAB_DB")
+	}
+	return "securechat"
+}
